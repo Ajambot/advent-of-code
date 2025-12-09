6b45eb2fa7a5857cbf873c5a694ca36c7ce111bd
Martin Morales <mmorale5@lakeheadu.ca>
2025-12-09 17:21:19 -0500

    Day 9 solution part 2. That was hard

--- 2025/9b.py
+++ 2025/9b.py
@@ -1,75 +1,79 @@
-with open('9test.txt') as f:
+def checkValid(points, i, o):
+    c1x, c1y = points[i]
+    c2x, c2y = points[o]
+    # lu corner case
+    if c1x <= c2x and c1y <= c2y:
+        cur = i+1
+        curx, cury = points[cur%len(points)]
+        while cur%len(points) != o:
+            if (c1x < curx < c2x) and (c1y < cury < c2y):
+                return False
+            prevx, prevy = points[(cur-1)%len(points)]
+            if ((prevx <= c1x and curx >= c2x) or (prevx >= c2x and curx <= c1x)) and (c1y < cury < c2y):
+                return False
+            if ((prevy <= c1y and cury >= c2y) or (prevy >= c2y and cury <= c1y)) and (c1x < curx < c2x):
+                return False
+            cur += 1
+            curx, cury = points[cur%len(points)]
+    # ru corner case
+    elif c1x >= c2x and c1y <= c2y:
+        cur = i+1
+        curx, cury = points[cur%len(points)]
+        while cur%len(points) != o:
+            if (c2x < curx < c1x) and (c1y < cury < c2y):
+                return False
+            prevx, prevy = points[(cur-1)%len(points)]
+            if ((prevx >= c1x and curx <= c2x) or (prevx <= c2x and curx >= c1x)) and (c1y < cury < c2y):
+                return False
+            if ((prevy <= c1y and cury >= c2y) or (prevy >= c2y and cury <= c1y)) and (c2x < curx < c1x):
+                return False
+            cur += 1
+            curx, cury = points[cur%len(points)]
+    # rd corner case
+    elif c1x >= c2x and c1y >= c2y:
+        cur = i+1
+        curx, cury = points[cur%len(points)]
+        while cur%len(points) != o:
+            if (c2x < curx < c1x) and (c2y < cury < c1y):
+                return False
+            prevx, prevy = points[(cur-1)%len(points)]
+            if ((prevx >= c1x and curx <= c2x) or (prevx <= c2x and curx >= c1x)) and (c2y < cury < c1y):
+                return False
+            if ((prevy >= c1y and cury <= c2y) or (prevy <= c2y and cury >= c1y)) and (c2x < curx < c1x):
+                return False
+            cur += 1
+            curx, cury = points[cur%len(points)]
+
+    # ld corner case
+    elif c1x <= c2x and c1y >= c2y:
+        cur = i+1
+        curx, cury = points[cur%len(points)]
+        while cur%len(points) != o:
+            if (c1x < curx < c2x) and (c2y < cury < c1y):
+                return False
+            prevx, prevy = points[(cur-1)%len(points)]
+            if ((prevx <= c1x and curx >= c2x) or (prevx >= c2x and curx <= c1x)) and (c2y < cury < c1y):
+                return False
+            if ((prevy >= c1y and cury <= c2y) or (prevy <= c2y and cury >= c1y)) and (c1x < curx < c2x):
+                return False
+            cur += 1
+            curx, cury = points[cur%len(points)]
+    return True
+
+
+with open('9.txt') as f:
     points = []
     for line in f:
         x, y = line.strip().split(',')
         points.append((int(x), int(y)))
 
     maxA = 0
-    dirx = 0
-    diry = 0
-    st = [(points[0][0], points[0][1])]
-    for i in range(1, len(points)*2):
-        x, y = points[i%len(points)]
-        if dirx == 0 and st[-1][0] != x: 
-                dirx = 1 if x > st[-1][0] else -1
-                st.append((x, st[-1][1]))
-        elif diry == 0 and st[-1] != y: 
-                diry = 1 if y > st[-1][1] else -1
-                st.append((st[-1][0], y))
+    for i in range(len(points)):
+        for o in range(i+1, len(points)):
+            c1x, c1y = points[i]
+            c2x, c2y = points[o]
+            area = (abs(c1x-c2x)+1) * (abs(c1y-c2y)+1)
+            if checkValid(points, i, o) and checkValid(points, o, i):
+                maxA = max(maxA, area)
 
-        if (dirx > 0 and x > st[-1][0]) or (dirx < 0  and x < st[-1][0]):
-            st.append((x, st[-1][1]))
-        else:
-            if dirx > 0:
-                corner = st.pop()
-                while st and st[-1][0] > x:
-                    area = (abs(corner[0]-st[-1][0]) + 1) * (abs(corner[1]-st[-1][1]) + 1)
-                    st.pop()
-                    maxA = max(maxA, area)
-                if st:
-                    area = (abs(corner[0]-x) + 1) * (abs(corner[1]-st[-1][1]) + 1)
-                    maxA = max(maxA, area)
-                else:
-                    dirx *= -1
-            else:
-                corner = st.pop()
-                while st and st[-1][0] < x:
-                    area = (abs(corner[0]-st[-1][0]) + 1) * (abs(corner[1]-st[-1][1]) + 1)
-                    st.pop()
-                    maxA = max(maxA, area)
-
-                if st:
-                    area = (abs(corner[0]-x) + 1) * (abs(corner[1]-st[-1][1]) + 1)
-                    maxA = max(maxA, area)
-                else:
-                    dirx *= -1
-            st.append((x, st[-1][1] if st else y))
-
-        if (diry > 0 and y > st[-1][0]) or (diry < 0  and y < st[-1][0]):
-            st.append((y, st[-1][1]))
-        else:
-            if diry > 0:
-                corner = st.pop()
-                while st and st[-1][0] > y:
-                    area = (abs(corner[0]-st[-1][0]) + 1) * (abs(corner[1]-st[-1][1]) + 1)
-                    st.pop()
-                    maxA = max(maxA, area)
-                if st:
-                    area = (abs(corner[0]-st[-1][0]) + 1) * (abs(corner[1]-y) + 1)
-                    maxA = max(maxA, area)
-                else:
-                    diry *= -1
-            else:
-                corner = st.pop()
-                while st and st[-1][0] < y:
-                    area = (abs(corner[0]-st[-1][0]) + 1) * (abs(corner[1]-st[-1][1]) + 1)
-                    st.pop()
-                    maxA = max(maxA, area)
-                if st:
-                    area = (abs(corner[0]-st[-1][0]) + 1) * (abs(corner[1]-y) + 1)
-                    maxA = max(maxA, area)
-                else:
-                    diry *= -1
-            st.append((st[-1][0] if st else x, y))
-        print(x, y, maxA)
     print(maxA)
